54b7c85a3bf5ca1369422c56962de64bb1258c4c
Arya V L <aryalekshmi9813@gmail.com>
2022-08-09 21:40:58 +0530

    Added Navbar|Intro|Responsible

---
+++ src/components/Intro/Intro.jsx
@@ -0,0 +1,115 @@
+import React from 'react'
+import Paper from '@material-ui/core/Paper'
+import {makeStyles} from '@material-ui/core/styles'
+import Container from '@material-ui/core/Container'
+import Grid from '@material-ui/core/Grid'
+import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
+import Box from '@material-ui/core/Box'
+import Social from '../Intro/Social'
+import Hidden from '@material-ui/core/Hidden'
+import Zoom from '@material-ui/core/Zoom'
+import { useState } from 'react'
+import { useEffect } from 'react'
+import DownloadIcon from '@material-ui/icons/CloudDownloadOutlined'
+
+
+
+const  useStyles = makeStyles((theme)=>({
+section:{
+   
+    height:'90vh',
+   position:'relative',
+},
+content:{
+    height:'100%',
+    zIndex:'100',
+    position:'relative',
+},
+container:{
+    height:'100%',
+    
+},
+
+subTitle:{
+    padding:'20px 0',
+},
+btn:{
+    width:'12rem',
+    color:'white',
+  padding:' 0.5rem 2rem',
+  borderRadius: '34px',
+  fontSize: '16px',
+  border: 'none',
+  outline:'none',
+  background: 'linear-gradient(180deg,#fdc50f 26.71%, #fb982f 99.36%)',
+  boxShadow:'0px 20px 24px 3px rgba(251,161,40,0.42)' ,
+  transition:' all 0.5s ease-out',
+  marginBottom:'40px',
+ 
+    // height:' 2rem',
+    '&:hover':{
+        background: 'white',
+        
+        color: 'orange',
+        border:' 1px solid orange',
+    }
+},
+resume:{
+    textDecoration:'none',
+    color:'white',
+    fontWeight:'bold',
+    display:'flex',
+    justifyContent:'space-between',
+    gap:'10px',
+    '&:hover':{
+        color:'orange'
+    }
+}
+
+
+
+}))
+const Intro = () => {
+    const styles=useStyles()
+    const [shouldShow,setShouldShow]=useState(false)
+    useEffect(()=>
+        setShouldShow(true)
+    )
+  return (
+    <Paper className={styles.section} id='home'>
+       
+       
+        <Container className={styles.container} maxWidth='md'>
+        <Grid className={styles.content} container alignItems='center' justifyContent='space-between'>
+       <Zoom in={shouldShow}>
+        <Grid item sm={8}>
+        <Typography component='h1' variant='h3'>
+            Hi, My name is Arya. I'm a Frontend Developer
+        </Typography>
+            <Typography variant='h5' className={styles.subTitle}>I build web applications and responsive user interfaces. </Typography>
+        <Box my={2}>
+
+        <Button href='mailto:aryalekshmi9813@gmail.com' variant='outlined'  className={styles.btn}>
+            <a href='#' download='Arya-ReactJS.pdf' className={styles.resume}>Resume <DownloadIcon className={styles.resumeIcon}/></a>
+        </Button>
+        <Hidden smUp>
+        <Grid item >
+            <Social direction={'row'}/>
+        </Grid>
+        </Hidden>
+        </Box>
+        </Grid>
+        </Zoom>
+        <Hidden xsDown>
+        <Grid item>
+            <Social direction={'column'}/>
+        </Grid>
+        </Hidden>
+        </Grid>
+        </Container>
+    </Paper>
+  )
+}
+
+export default Intro
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,109 @@
+import React from 'react'
+import  Container  from '@material-ui/core/Container'
+import Typography  from '@material-ui/core/Typography'
+import Link from '@material-ui/core/Link'
+import  AppBar  from '@material-ui/core/AppBar'
+import  {makeStyles}  from '@material-ui/core/styles'
+import  Toolbar  from '@material-ui/core/Toolbar'
+import Hidden from '@material-ui/core/Hidden'
+import IconButton from '@material-ui/core/IconButton'
+import MenuIcon from '@material-ui/icons/Menu'
+import  SwipeableDrawer  from '@material-ui/core/SwipeableDrawer'
+import ChevronRightIcon from '@material-ui/icons/ChevronRight'
+import Divider from '@material-ui/core/Divider'
+import List from '@material-ui/core/List'
+import  ListItem  from '@material-ui/core/ListItem'
+import { useState } from 'react'
+import DownloadIcon from '@material-ui/icons/ArrowDownward'
+
+const navigationLinks=[
+    {name:'Home',href:'#home',},
+    {name:'Skills',href:'',},
+    {name:'Projects',href:'#projects',},
+    {name:'Education',href:'',},
+    {name:'Certificates',href:'',},
+    {name:'Contact',href:'',}
+
+]
+
+const useStyles=makeStyles((theme)=>({
+    navLeft:{
+        marginRight:'auto',
+    },
+    link:{
+        marginRight:20,
+        cursor:'pointer'
+    },
+    icon:{
+        position:'absolute',
+        top:'10px',
+       right:'20px',
+    },
+    list:{
+        marginTop:"40px",
+    },
+   
+}))
+
+// Main navbar starts
+const Navbar = () => {
+    const styles=useStyles();
+    const [open,setOpen]=useState(false)
+  return (
+    <AppBar position='sticky' color='default' >
+    <Container maxWidth='md'>
+        <Toolbar>
+        <div className={styles.navLeft}>
+        <Typography >Arya</Typography>
+        </div>
+        <Hidden xsDown>
+        {navigationLinks.map((item)=>(
+           
+                <Link key={item.href}
+                color='textPrimary'
+                variant='button'
+                underline='none'
+                className={styles.link}
+                >{item.name}</Link>
+            
+        ))}
+        </Hidden>
+        <Hidden smUp>
+            <IconButton>
+                <MenuIcon onClick={()=>setOpen(true)}/>
+            </IconButton>
+        </Hidden>
+        </Toolbar>
+    </Container>
+    <SwipeableDrawer position='relative' anchor='right' open={open} onOpen={()=>setOpen(true)} onClose={()=>setOpen(false)}>
+        <div className={styles.icon}>
+            <IconButton>
+                <ChevronRightIcon onClick={()=>setOpen(false)}/>
+            </IconButton>
+        </div>
+        <div className="divider">
+
+        <Divider/>
+        </div>
+        <List className={styles.list}>
+
+       
+        {navigationLinks.map((item)=>(
+            <ListItem>
+            <Link key={item.href} 
+            color='textPrimary'
+            
+            variant='button'
+            underline='none'
+            className={styles.link}
+            >{item.name}</Link>
+            </ListItem>
+
+        ))}
+        </List>
+    </SwipeableDrawer>
+    </AppBar>
+  )
+}
+
+export default Navbar
